ea47946e4d3a20edb04c1c482416bc5a3aceb3f1
yxfeng <yxfeng@thoughtworks.com>
2019-12-06 11:09:05 +0800

    initialize project

---
+++ src/test/java/com/ff/yyy/micrometer/MicrometerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ff.yyy.micrometer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrometerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
